f22c899a1d92509edd94f00fe2e2990f78816e5e
Anna Sychikova <asychikova@myseneca.ca>
2024-07-29 19:22:18 -0400

    minor changes

--- nasa-app/src/components/Nav.js
+++ nasa-app/src/components/Nav.js
@@ -6,7 +6,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
-import styles from "@/Styles/navbar.module.css";
+import styles from "../Styles/navbar.module.css";
 
 export default function NavBar() {
   return (
