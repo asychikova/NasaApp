20d7802c615f40df856952bfa80b6512f7101b7b
Mr Khaira <singhkhairaharkaran@gmail.com>
2024-08-01 23:47:15 -0400

    Change in registration design, added node js server.

--- nasa-app/src/app/auth/register/page.js
+++ nasa-app/src/app/auth/register/page.js
@@ -1,55 +1,90 @@
 "use client";
 import React from "react";
+import { useState } from "react";
 import styles from "@/Styles/form.module.css";
 
 export default function Home() {
-  function handleRegister() {
-    console.log("Register the user, call the specific API.");
+  const [formData, setFormData] = useState({
+    userName: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  async function handleRegister(e) {
+    e.preventDefault();
+    setError(""); // Reset any previous errors
+    try {
+      const resp = await fetch("http://localhost:3000/user/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(e),
+      });
+
+      if (response.ok) {
+        console.log("User registered successfully");
+      } else {
+        console.log("Error registering user");
+      }
+    } catch (error) {
+      console.log("Error in user registration:", error);
+      setError("An unexpected error occurred. Please try again.");
+    }
   }
   return (
-    <>
-      <form onSubmit={handleRegister} className={styles.mainContainor}>
-        <div className={styles.containor}>
-          <h3 style={{ alignSelf: "center", paddingBottom: "30px" }}>
-            Create a new account
-          </h3>
-          <label htmlFor="username">
+    <div className="container mt-5">
+      <form onSubmit={handleRegister} className="w-50 mx-auto p-5 bg-white">
+        <h3 className="text-center mb-4">Create a new account</h3>
+        {error && (
+          <p className="text-center mb-4" style={{ color: "red" }}>
+            {error}
+          </p>
+        )}
+        <div className="d-flex flex-column align-items-center">
+          <div className="mb-4" style={{ width: "100%" }}>
             <input
               type="text"
-              id="username"
-              name="username"
+              name="userName"
               placeholder="Username"
-              className={styles.input}
+              value={formData.value}
+              onChange={handleChange}
+              className={`form-control ${styles.input}`}
               required
             />
-          </label>
-          <br />
-          <label htmlFor="email">
+          </div>
+          <div className="mb-4" style={{ width: "100%" }}>
             <input
               type="email"
-              id="email"
               name="email"
               placeholder="Email"
-              className={styles.input}
+              value={formData.email}
+              onChange={handleChange}
+              className={`form-control ${styles.input}`}
               required
             />
-          </label>
-          <br />
-          <label htmlFor="password">
+          </div>
+          <div className="mb-4" style={{ width: "100%" }}>
             <input
               type="password"
-              id="password"
               name="password"
               placeholder="Password"
-              className={styles.input}
+              value={formData.password}
+              onChange={handleChange}
+              className={`form-control ${styles.input}`}
               required
             />
-          </label>
-          <button className="w-50 btn btn-dark" style={{ margin: "15px" }}>
+          </div>
+          <button className="btn btn-dark w-100" style={{ margin: "15px" }}>
             Register
           </button>
         </div>
       </form>
-    </>
+    </div>
   );
 }
