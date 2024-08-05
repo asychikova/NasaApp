060061b786747712775a2c7dd5b635abc41d34a9
Mr Khaira <singhkhairaharkaran@gmail.com>
2024-08-05 04:38:02 -0400

    /user/verify-email, made better.

--- nasa-app/src/app/auth/register/page.js
+++ nasa-app/src/app/auth/register/page.js
@@ -23,13 +23,16 @@ export default function Home() {
 
     //"https://nasa-app-server-p2d3.onrender.com/user/register"
     try {
-      const resp = await fetch("http://localhost:3001/user/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+      const resp = await fetch(
+        "https://nasa-app-server-p2d3.onrender.com/user/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
 
       const result = await resp.json();
       if (resp.ok) {
